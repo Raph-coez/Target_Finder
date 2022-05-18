9e29e889f568f39b22169df71601a3ac7a88fa0a
Raphael COEZ <raphael.coez@cpe.fr>
2022-05-18 11:31:10 +0200

    commit

--- Program/src/header.py
+++ Program/src/header.py
@@ -16,8 +16,10 @@ import os
 from math import exp,log
 
 # variables gloables
+
 WIDTH_FRAME = 512
 PROCESSED = False
+PROCESSED_2 = False
 COUNT = 0
 SEARCHING_TARGET = True
 LUMIERE_TRUE = "L"
@@ -27,8 +29,12 @@ ANGLE = 278
 ANGLE_VERTICAL = 160
 
 
-# fonctions
 
+# fonctions
+def check_searching_target():
+    global SEARCHING_TARGET
+    a = SEARCHING_TARGET
+    return a
 def vertical_correction_value(pos,y,h):
     correction = 0
     mh = h/2
@@ -42,20 +48,22 @@ def vertical_correction_value(pos,y,h):
     return correction
 
 def horizontal_correction_value(x,w):
+    global WIDTH_FRAME
     correction = 0
     mw = w/2
     middle_target = x+int(mw)
     diff = int(WIDTH_FRAME/2) - middle_target
-    tolerance = 7
+    tolerance = 8
     if diff > tolerance:
-        correction += 2
+        correction = 1
     if diff < -1*tolerance:
-        correction -= 2
+        correction -= 1
     return correction
 
 def correction_verticale(pointeur_pos,y,h):
+    global ANGLE_VERTICAL
     angle = ANGLE_VERTICAL
-    if vertical_correction_value(y,h) != 0:
+    if vertical_correction_value(pointeur_pos,y,h) != 0:
         print('Correction verticale necessaire ...')
         new_angle = angle + vertical_correction_value(pointeur_pos,y,h)
         print('Nouvel angle vertical : ' + str(new_angle))
@@ -66,15 +74,31 @@ def correction_verticale(pointeur_pos,y,h):
         print('Arret de la correction')
         p = True
         return angle, p
+
+def correction_verticale_2(distance):
+    distance = int(distance)
+    inter = -1*distance/30
+    new_angle = int(inter)
+    new_angle += 170
+    str_new_angle = str(new_angle)
+    if new_angle < 100 and new_angle > 9:
+        str_new_angle = '0' + str(new_angle)
+    if new_angle < 10 and new_angle > 2:
+        str_new_angle = '00' + str(new_angle)
+    if new_angle < 3:
+        str_new_angle = '003'
+    return str_new_angle
     
 def correction_horizontale(im,x,w):
-
+    global ANGLE
     angle = ANGLE
+    p = False
     if horizontal_correction_value(x,w) != 0:
         print('Correction necessaire ...')
         new_angle = angle + horizontal_correction_value(x,w)
         print('Nouvel angle : ' + str(new_angle))
         p = False
+        ANGLE = new_angle
         return new_angle, p
     else:
         print('Correction non necessaire ...')
@@ -103,6 +127,7 @@ def print_camera(vs):
     return gray, frame
 
 def check_detection(detector, gray_img):
+    global COUNT
     rect = detector.detectMultiScale( gray_img, scaleFactor=1.25, minNeighbors=1, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
     if rect == ():
         detected = False
@@ -115,6 +140,8 @@ def check_detection(detector, gray_img):
     return detected, rect
 
 def set_data(rect,frame):
+    global DATA
+    distance = 100
     for (fX, fY, fW, fH) in rect:
         DATA = [fX,fW,fY,fH]
         cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
@@ -122,14 +149,19 @@ def set_data(rect,frame):
         return distance
 
 def vertical_target(pointeur_pos, detect, ser):
+    global DATA
+    global PROCESSED_2
+    global SEARCHING_TARGET
+    global LUMIERE_FALSE
+    global LUMIERE_TRUE
+    global ANGLE_VERTICAL
     if (detect):
         sleep(0.1)
-        im = cv2.imread('..\img\img.png',0)
         y,h = DATA[2],DATA[3]
-        angle, PROCESSED = correction_verticale(pointeur_pos,y,h)
+        angle, PROCESSED_2 = correction_verticale(pointeur_pos,y,h)
         consigne = str(angle) + 'F'
         ser.write(consigne.encode())
-        if PROCESSED:
+        if PROCESSED_2:
             print('Cible atteinte ***********')
             print('Arret de la correction')
             ser.write(LUMIERE_FALSE.encode())
@@ -140,27 +172,46 @@ def vertical_target(pointeur_pos, detect, ser):
             if (angle < 3 or angle > 176):
                 print('Angle maximal atteint')
             else:
-                ANGLE = angle
-                print('Angle modifé : ' + str(angle))
-            
+                ANGLE_VERTICAL = angle
+    
+def vertical_target_2(distance, ser):
+    global DATA
+    global PROCESSED_2
+    global SEARCHING_TARGET
+    global LUMIERE_FALSE
+    global LUMIERE_TRUE
+    global ANGLE_VERTICAL
+    sleep(0.1)
+    angle = correction_verticale_2(distance)
+    consigne = str(angle) + 'F'
+    ser.write(consigne.encode()) 
+
 def horizontal_target(detected,ser):
+    global DATA
+    global PROCESSED
+    global LUMIERE_TRUE
+    global ANGLE
+    b = False
     if(detected):
         sleep(0.1)
         im = cv2.imread('..\img\img.png',0)
         x,w = DATA[0],DATA[1]
-        angle, PROCESSED = correction_horizontale(im,x,w)
+        angle, b = correction_horizontale(im,x,w)
         consigne = str(angle) + 'F'
         ser.write(consigne.encode())
-        if PROCESSED:
-            print('Position horizontale atteinte')
-            print("Arret de la correction d'angle horizontal")
+        if b:
+            print('Cible atteinte ***********')
+            print('Arret de la correction')
             ser.write(LUMIERE_TRUE.encode())
+            global SEARCHING_TARGET
+            SEARCHING_TARGET = False
         else:
             if (angle < 183 or angle > 356):
                 print('Angle maximal atteint')
             else:
                 ANGLE = angle
                 print('Angle modifé : ' + str(angle))
+    return b
 
 def TOR_Pointeur(img): 
     format = np.shape(img)
@@ -168,7 +219,7 @@ def TOR_Pointeur(img):
     for i in range(format[0]):
         for j in range(format[1]):
             gray_pixel = img[i, j]
-            if ((gray_pixel < 250)):
+            if ((gray_pixel < 254)):
                 new_img[i, j] = 0
             else:
                 new_img[i, j] = 255
@@ -211,6 +262,7 @@ def get_pointeur_position(img):
 def get_pointeur_center(img):
     
     # processing et affichage console
+    center = (0,0)
     print('Demarrage du programme de detection de pointeur --------')
     processed_img = TOR_Pointeur(img)
     detect = check_if_pixel_blanc(processed_img)
@@ -229,3 +281,4 @@ def get_pointeur_center(img):
     cv2.imshow('img',img)
     cv2.imshow('processed_img',processed_img)
     return center, detect
+
--- Program/src/main.py
+++ Program/src/main.py
@@ -9,16 +9,21 @@ detector = get_detector()
 ser = serial.Serial('COM5',19200)
 consignes_init = [str(ANGLE)+'F',LUMIERE_FALSE,str(ANGLE_VERTICAL)+'F']
 init_position(ser,consignes_init,"Remise a l'angle initial vertical et horizontal, exctinction de la lumiere")
+p = False
 
 # Loop
-while SEARCHING_TARGET:
+while check_searching_target():
 	COUNT += 1
-	gray_img, frame = print_camera()
+	gray_img, frame = print_camera(vs)
 	detected, rect = check_detection(detector, gray_img)
 	distance = set_data(rect, frame)
-	horizontal_target(detected, ser)
-	pointeur_pos, detect = get_pointeur_center(gray_img)
-	vertical_target(pointeur_pos,detect, ser)
+	if not p:
+		p = horizontal_target(detected, ser)
+	#if p: 
+		#pointeur_pos, detect = get_pointeur_center(gray_img)
+		#vertical_target(pointeur_pos,detect, ser)
+	if detected:
+		vertical_target_2(distance, ser)
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
